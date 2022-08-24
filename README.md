46597b198ecc974619df22f038f2080d48985da4
Robert James Gabriel <robert_gabriel@outlook.com>
2022-08-24 16:46:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Helperbird website [![Build and Deploy](https://github.com/Helperbird/website/actions/workflows/build.yml/badge.svg)](https://github.com/Helperbird/website/actions/workflows/build.yml)
+
+## Getting started
+
+### Install assets
+
+```bash
+npm install
+```
+
+### Build files
+
+```bash
+npm run build
+```
+
+### Run locally
+
+```bash
+npm run dev
+```
---
+++ docs/service-worker.js
@@ -0,0 +1,93 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
+
+workbox.core.setCacheNameDetails({prefix: "eleventy-plugin-pwa"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "7d9b6445d97720c98886f37bd9df923d"
+  },
+  {
+    "url": "404.png",
+    "revision": "e94ce63b35a57b3e639ad0d5707a38ea"
+  },
+  {
+    "url": "assets/css/blog.css",
+    "revision": "ee7c1673f5dce3c32e8da8729faed551"
+  },
+  {
+    "url": "assets/css/help.css",
+    "revision": "038c63ab6cb35811940314d495377eb0"
+  },
+  {
+    "url": "assets/css/styles.css",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "assets/js/core/app.js",
+    "revision": "a098280a40e759894517be888645ed20"
+  },
+  {
+    "url": "assets/js/core/third-party.js",
+    "revision": "ba449c1311e30858aa34c8c9d601df05"
+  },
+  {
+    "url": "assets/js/core/youtube.js",
+    "revision": "01c5fa9ef1159467c6823c275882179f"
+  },
+  {
+    "url": "assets/js/index.js",
+    "revision": "9c4909346822294493f76ae9436e2847"
+  },
+  {
+    "url": "assets/js/main.bundle.js",
+    "revision": "76cc8d3163088a05c25dd1a2ef08602a"
+  },
+  {
+    "url": "assets/js/main.css",
+    "revision": "4d4da09dcee52455ec112adc041014b5"
+  },
+  {
+    "url": "assets/videos/HBmenu1.png",
+    "revision": "811e05d43e9c331f752dd97e9c271dd1"
+  },
+  {
+    "url": "assets/videos/HBmenu2.png",
+    "revision": "d6fa4c050eacc3df39edfa231363031d"
+  },
+  {
+    "url": "index.html",
+    "revision": "3bb8fc66df7aa512ea384170cfc7cd93"
+  },
+  {
+    "url": "index.png",
+    "revision": "6c4f55d91b1f3675eec7136d021b2166"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^.*\.(html|jpg|png|gif|webp|ico|svg|woff2|woff|eot|ttf|otf|ttc|json)$/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
